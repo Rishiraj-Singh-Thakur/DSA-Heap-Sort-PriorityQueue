2554b2b70d96adaf4f2fcb66b01f15ebbdb5429e
Rishiraj Singh Thakur <you@example.com>
2026-05-27 15:56:07 +0530

    practicing

--- HeapSortAlgorithm.java
+++ HeapSortAlgorithm.java
@@ -1,41 +1,45 @@
 public class HeapSortAlgorithm {
-    public static void heapify(int i , int size , int arr[]){
+    public static void heapify(int i , int [] arr , int size) {
         int left = 2*i+1;
         int right = 2*i+2;
         int maxIdx = i;
 
-        if(left <size && arr[left] > arr[maxIdx]){
+        if(left<size && arr[left] > arr[maxIdx]){
+        // if(left<size && arr[left] < arr[maxIdx]){ for decreasing order
+
             maxIdx = left;
         }
-        if(right <size && arr[right]>arr[maxIdx]){
+        if(right<size && arr[right]> arr[maxIdx]){
+        // if(right<size && arr[right]> arr[maxIdx]){ for decreasing order
+
             maxIdx = right;
         }
         if(maxIdx !=i){
             int temp = arr[i];
             arr[i] = arr[maxIdx];
             arr[maxIdx] = temp;
-            heapify(maxIdx, size, arr);
+            heapify(maxIdx, arr, size);
         }
     }
-    public static void HeapSort(int arr[]){
-        int n = arr.length;
-        // make Max heap
-        for(int i=n/2; i>=0;i--){
-            heapify(i ,n , arr);
+    public static void HeapSort(int[] arr) {
+        int size = arr.length;
+        for(int i=size/2;i>=0;i--){
+            heapify(i , arr , size);
         }
+
         // sorting
-        for(int i = n-1; i>=0;i--){
+        for(int i=size-1;i>=0;i--){
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
-            heapify(0, i, arr);
+            heapify(0, arr, i);
         }
     }
-public static void main(String[] args) {
-    int arr [] = {3,2,4,8,6,5,1,0};
-    HeapSort(arr);
-    for(int i = 0; i<arr.length;i++){
-        System.out.print(arr[i]+" ");
+    public static void main(String[] args) {
+        int arr [] = {3,2,4,8,6,5,1,0};
+        HeapSort(arr);
+        for(int i = 0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
-}
 }
